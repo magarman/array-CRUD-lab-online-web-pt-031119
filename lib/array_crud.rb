11699b6bd8a853a0cba797344354d65ba1c1a567
Mariana Garman <garman.mariana@gmail.com>
2019-03-12 01:41:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["wow", "I", "am", "really", "learning"]
-  << "arrays!"
+  ["wow", "I", "am", "really", "learning"] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
