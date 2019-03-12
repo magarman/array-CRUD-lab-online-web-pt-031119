8b3bd8010db1b4cf4b84014e48c9dd488853623b
Mariana Garman <garman.mariana@gmail.com>
2019-03-12 01:44:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["I", "am", "really", "learning"] 
+  ["I", "am", "really", "learning"] .unshift"wow"
 end
 
 def remove_element_from_end_of_array(array)
